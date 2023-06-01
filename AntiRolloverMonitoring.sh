d18013c752da4fbc16ee1d1e5e1ada2f9e6553d3
ericlai <laiyichengyuntech@gmail.com>
2023-06-01 14:01:15 +0800

    Update subproject4 script

--- AntiRolloverMonitoring.sh
+++ AntiRolloverMonitoring.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# IMU
-gnome-terminal -t "Navigation" -x bash -c 'source ~/common_ws/devel/setup.bash;roslaunch realsense2_camera IMU_Camera.launch'
-sleep 1
-
 # Subproject4
 gnome-terminal -t "Realsense" -x bash -c 'source ~/common_ws/devel/setup.bash;roslaunch forklift_server AntiRolloverMonitoring.launch'
 sleep 1
